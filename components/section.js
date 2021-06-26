37f7c90b14735ace2c115071c819b2a4a9db1fcf
Joel A. Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2021-06-26 17:40:10 -0300

    feat(ui): section with optional title

--- components/section.js
+++ components/section.js
@@ -1,7 +1,7 @@
 import { Box, Heading } from '@chakra-ui/react';
 
-const Section = ({ title, children }) => <Box>
-    <Heading mb={3} as="h2" variant="sectionTitle">{title}</Heading>
+const Section = ({ title = '', children }) => <Box>
+    {title ? <Heading mb={3} as="h2" variant="sectionTitle">{title}</Heading> : ''}
     {children}
 </Box>;
 
